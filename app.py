b790f5761ec945c482d4437b5b97115b57be630d
icfarias <149179034+icfarias@users.noreply.github.com>
2025-07-21 20:46:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -136,12 +136,13 @@ def run_random_forest(df):
         ('classifier', RandomForestClassifier(n_estimators=3, class_weight='balanced', random_state=42))
     ])
     X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.2, random_state=42)
+    clf.fit(X_train, y_train)
+
     y_test = np.array(y_test).ravel()
     y_pred = np.array(y_pred).ravel()
     if len(np.unique(y_test)) < 2:
         st.error("Não há ao menos duas classes na variável alvo para avaliar a matriz de confusão! Verifique os filtros e dados de entrada.")
         return None, None, None, None
-    clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     cm = confusion_matrix(y_test, y_pred)
     report = classification_report(y_test, y_pred, output_dict=True)
