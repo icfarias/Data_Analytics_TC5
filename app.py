f6597c52392089485fe227e78e973ab52d7ee9ab
icfarias <149179034+icfarias@users.noreply.github.com>
2025-07-22 22:10:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -104,25 +104,20 @@ def combine_dataframes(jobs_data, prospects_data, applicants_data):
 def run_random_forest(df):
     df_model = df.copy()
     # Remova TUDO o que é textual ou categórico largo (adaptar conforme necessidade)
-    # cols_to_drop = [
-    #     'main_activities_job', 'technical_skills_job',
-    #     'prospect_id', 'prospect_name', 'prospect_comment',
-    #     'prospect_status', 'applicant_id', 'technical_knowledge',
-    #     'client', 'job_id',    # IDs/clientes, não agregam e podem explodir NaNs
-    #     'area_of_expertise',   # texto potencialmente amplo
-    #     'academic_level'
-    # ]
     cols_to_drop = [
         'main_activities_job', 'technical_skills_job',
         'prospect_id', 'prospect_name', 'prospect_comment',
-        'prospect_status', 'applicant_id', 'technical_knowledge'
+        'prospect_status', 'applicant_id', 'technical_knowledge',
+        'client', 'job_id',    # IDs/clientes, não agregam e podem explodir NaNs
+        'area_of_expertise',   # texto potencialmente amplo
+        'academic_level'
     ]
     # Só mantenha as colunas alvo e poucas cols categóricas de valor reduzido
     cols_to_keep = [c for c in df_model.columns if c not in cols_to_drop and c != 'is_hired']
     # Mantém apenas colunas com poucos valores únicos (<15), exceto numericas
     small_cat_cols = [
         c for c in cols_to_keep
-        if (df_model[c].dtype == "object" and df_model[c].nunique() < 50) or
+        if (df_model[c].dtype == "object" and df_model[c].nunique() < 30) or
         (df_model[c].dtype != "object")
     ]
     df_model = df_model[small_cat_cols + ['is_hired']]
@@ -154,7 +149,7 @@ def run_random_forest(df):
     ])
     clf = Pipeline(steps=[
         ('preprocessor', preprocessor),
-        ('classifier', RandomForestClassifier(n_estimators=5, class_weight='balanced', random_state=42))
+        ('classifier', RandomForestClassifier(n_estimators=4, class_weight='balanced', random_state=42))
     ])
     X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.4, random_state=42)
 
