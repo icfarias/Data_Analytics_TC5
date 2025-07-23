0e5a5206f53216380962242db08f71d5fed187b5
icfarias <149179034+icfarias@users.noreply.github.com>
2025-07-22 22:56:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ st.sidebar.markdown(integrantes_md, unsafe_allow_html=True)
 
 # ---------- FUNÇÕES AUXILIARES ------------
 
-LIMIT_APPLICANTS = 500  # Bem agressivo para garantir performance
+LIMIT_APPLICANTS = 1000  
 
 def load_json_upload(uploaded_json, limit_applicants=False):
     data = json.load(uploaded_json)
@@ -113,10 +113,10 @@ def run_random_forest(df):
     ]
     # Só mantenha as colunas alvo e poucas cols categóricas de valor reduzido
     cols_to_keep = [c for c in df_model.columns if c not in cols_to_drop and c != 'is_hired']
-    # Mantém apenas colunas com poucos valores únicos (<30)
+    # Mantém apenas colunas com poucos valores únicos (<20)
     small_cat_cols = [
     c for c in cols_to_keep
-    if (df_model[c].dtype == "object" and df_model[c].nunique() < 30) or
+    if (df_model[c].dtype == "object" and df_model[c].nunique() < 20) or
        (df_model[c].dtype != "object") or
        (c == 'area_of_expertise')  
     ]
